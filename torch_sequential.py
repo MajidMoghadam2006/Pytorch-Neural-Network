813ff03cedc49e03955972cf3f8ca3005eacc472
Majid <majid.moghadam2006@gmail.com>
2020-04-11 14:51:51 -0700

    cuda training + save model

--- torch_sequential.py
+++ torch_sequential.py
@@ -7,30 +7,37 @@ class Classifier():
         self.model = model
         self.criterion = criterion
         self.optimizer = optimizer
+        self.cuda = cuda
          
-        if cuda:
+        if self.cuda:
             self.model.cuda()
 
     def fit(self, trainloader, testloader=None, epochs=5, print_every=1):
         ''' train on the trainloader dataset and validate on testloader dataset'''
+        valid_loss_min = float('inf') # track change in validation loss
         steps = 0
         for e in range(epochs):
             # Model in training mode, dropout is on
             self.model.train()
             running_loss = 0
-            for images, labels in trainloader:
+            for data, target in trainloader:
+                
+                # move tensors to GPU if CUDA is available
+                if self.cuda:
+                    data, target = data.cuda(), target.cuda()
+                
                 # Batch training
                 steps += 1
 
                 # Flatten images into a input_size long vector => images: batch_size * input_size
-                images.resize_(images.size()[0], images.size()[-1]*images.size()[-2])
+                data.resize_(data.size()[0], data.size()[-1]*data.size()[-2])
                 
                 # set grads to zero to avoid accumulating them
                 self.optimizer.zero_grad()
 
                 # forward pass +  backpropagation + update weights
-                output = self.model.forward(images)
-                loss = self.criterion(output, labels)
+                output = self.model.forward(data)
+                loss = self.criterion(output, target)
                 loss.backward()
                 self.optimizer.step()
 
@@ -51,24 +58,55 @@ class Classifier():
                       "Test Accuracy: {:.3f}".format(accuracy))
 
     def evaluate(self, testloader):
+        '''evaluate model accuracy and loss on test/validation dataset'''
         accuracy = 0
         test_loss = 0
-        for images, labels in testloader:
+        for data, target in testloader:
+            
+            # move tensors to GPU if CUDA is available
+            if self.cuda:
+                data, target = data.cuda(), target.cuda()
+            
             # Flatten images into a input_size long vector => images: batch_size * input_size
-            images.resize_(images.size()[0], images.size()[-1]*images.size()[-2])
+            data.resize_(data.size()[0], data.size()[-1]*data.size()[-2])
 
-            output = self.model.forward(images)
-            test_loss += self.criterion(output, labels).item()
+            output = self.model.forward(data)
+            test_loss += self.criterion(output, target).item()
 
             ## Calculating the accuracy
             # Model's output is log-softmax, take exponential to get the probabilities
             ps = torch.exp(output)
             # Class with highest probability is our predicted class, compare with true label
-            equality = (labels.data == ps.max(1)[1])
+            equality = (target.data == ps.max(1)[1])
             # Accuracy is number of correct predictions divided by all predictions, just take the mean
             accuracy += equality.type_as(torch.FloatTensor()).mean()
 
         return test_loss/len(testloader), accuracy/len(testloader)
 
     def predict(self, testloader):
-        pass
+        '''generate model predictions (probability vector) for the input dataset'''
+        for data, target in testloader:
+            
+            # move tensors to GPU if CUDA is available
+            if self.cuda:
+                data, target = data.cuda(), target.cuda()
+            
+            # Flatten images into a input_size long vector => images: batch_size * input_size
+            data.resize_(data.size()[0], data.size()[-1]*data.size()[-2])
+
+            output = self.model.forward(data)
+            test_loss += self.criterion(output, target).item()
+
+            ## Calculating the accuracy
+            # Model's output is log-softmax, take exponential to get the probabilities
+            ps = torch.exp(output)
+        return ps
+    
+    def save_model(self, model_path):
+        '''save torch model in model_path.pt'''
+        torch.save(self.model.state_dict(), model_path + '.pt')
+        
+        
+        
+        
+        
